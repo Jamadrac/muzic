0c27b459936d10b94a25f53cba5d598821697fea
jamadrac <rabbikindalo001@gmail.com>
2023-12-12 10:12:09 +0200

    \

--- lib/models/song_model.dart
+++ lib/models/song_model.dart
@@ -1,3 +1,5 @@
+import 'package:file_picker/file_picker.dart';
+
 class Song {
   final String title;
   final String description;
@@ -11,24 +13,37 @@ class Song {
     required this.coverUrl,
   });
 
-  static List<Song> songs = [
-    Song(
-      title: 'Glass',
-      description: 'Glass',
-      url: 'assets/music/glass.mp3',
-      coverUrl: 'assets/images/glass.jpg',
-    ),
-    Song(
-      title: 'Illusions',
-      description: 'Illusions',
-      url: 'assets/music/illusions.mp3',
-      coverUrl: 'assets/images/illusions.jpg',
-    ),
-    Song(
-      title: 'Pray',
-      description: 'Pray',
-      url: 'assets/music/pray.mp3',
-      coverUrl: 'assets/images/pray.jpg',
-    )
-  ];
+  static List<Song> songs = [];
+
+  static Future<void> initializeSongs() async {
+    List<Song> loadedSongs = await _searchForMusic();
+    songs = loadedSongs;
+  }
+
+  static Future<List<Song>> _searchForMusic() async {
+    List<Song> songs = [];
+
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.audio,
+      allowMultiple: true,
+    );
+
+    if (result != null && result.files.isNotEmpty) {
+      songs = result.files.where((file) => !_isACCFile(file.path)).map((file) {
+        return Song(
+          title: file.name ?? 'Unknown Title',
+          description: 'Local audio file',
+          url: file.path ?? '',
+          coverUrl: 'path/to/your/default/cover/image.jpg',
+        );
+      }).toList();
+    }
+
+    return songs;
+  }
+
+  static bool _isACCFile(String filePath) {
+    // Check if the file path ends with .acc
+    return filePath.toLowerCase().endsWith('.aac');
+  }
 }
